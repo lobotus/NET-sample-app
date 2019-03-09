fb27098a5ca9e8b87d90b4b7dbe5552f00a3d91b
unknown <admin@lobotus.com>
2019-03-09 15:16:39 +0530

    9thMarch

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using ViewEnvironment;
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Assert.AreEqual("Hello",Default.message());
+        }
+    }
+}
